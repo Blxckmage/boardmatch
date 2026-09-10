435ed20a13f6db19c30cf1383544e84f23bf35b3
Faza Zikri <fazadoang15@gmail.com>
2026-09-10 03:47:33 +0000

    chore(format): prettier, reformat repo
    Refs: BLA-59

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -4,26 +4,26 @@ import * as Effect from "effect/Effect";
 import Backend from "./src/server/backend.ts";
 
 export const Website = Cloudflare.Website.Vite("Website", {
-  env: {
-    BACKEND: Backend,
-  },
+	env: {
+		BACKEND: Backend,
+	},
 });
 
 export type WebsiteEnv = Cloudflare.InferEnv<typeof Website>;
 
 export default Alchemy.Stack(
-  "boardmatch",
-  {
-    providers: Cloudflare.providers(),
-    state: Cloudflare.state(),
-  },
-  Effect.gen(function* () {
-    const backend = yield* Backend;
-    const website = yield* Website;
+	"boardmatch",
+	{
+		providers: Cloudflare.providers(),
+		state: Cloudflare.state(),
+	},
+	Effect.gen(function* () {
+		const backend = yield* Backend;
+		const website = yield* Website;
 
-    return {
-      backendUrl: backend.url.as<string>(),
-      websiteUrl: website.url.as<string>(),
-    };
-  }),
+		return {
+			backendUrl: backend.url.as<string>(),
+			websiteUrl: website.url.as<string>(),
+		};
+	}),
 );
